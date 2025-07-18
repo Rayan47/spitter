e6396e0c69f2c9d99b50214354912129a00d6f83
RayanSparsh <rayan.sm@sparsh.io>
2025-07-18 23:41:08 +0530

    removed users.ts

--- src/app/api/users/users.ts
+++
@@ -1,9 +0,0 @@
-import {initMongoose} from "../../../../lib/mongoose";
-import User from "../../../../models/UserSchema";
-
-export default async function handle(req, res){
-    await initMongoose();
-    const id = req.params.id;
-    const user = await User.findById(id);
-    res.json(user);
-}
